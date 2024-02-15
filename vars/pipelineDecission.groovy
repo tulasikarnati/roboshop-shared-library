e9ee83b4024ad97a300018c089b954969df0d7f1
Tulasi Karnati <karnatitulasi2099@gmail.com>
2024-02-15 19:01:44 +0000

    jenkins

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,25 +1,5 @@
 // #!groovy
 
-// def decidepipeline(Map, configMap) {
-//     application = configMap.get("application")
-//     switch(application) {
-//         case 'nodejsVM':
-//             nodejsVM(configMap)
-//             break
-//         case 'javaVM':
-//             javaVM(configMap)
-//             break
-//         case 'nodejsEKs':
-//             nodejsEKs(configMap)
-//             break
-//         default:
-//             error "Application is not recognized"
-//             break
-//     }
-// }
-
-#!groovy
-
 def decidePipeline(Map configMap){
     application = configMap.get("application")
     switch(application) {
